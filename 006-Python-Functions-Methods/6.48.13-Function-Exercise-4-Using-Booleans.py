ef296a2ded4fe8236ad485a9efde457493bacd4a
mdphx-mac <mdawsonphx@gmail.com>
2022-04-14 21:54:11 -0700

    Function Exercise #4: Using Booleans

---
+++ 006-Python-Functions-Methods/6.48.13-Function-Exercise-4-Using-Booleans.py
@@ -0,0 +1,10 @@
+# Coding Exercise #4 - Using Booleans
+
+# Define a function called myfunc that takes three arguements: x, y, and z.
+# If z is True, return x. If z is False, return y.
+
+def myfunc(x, y, z):
+    if z == True:
+        return x
+    else:
+        return y

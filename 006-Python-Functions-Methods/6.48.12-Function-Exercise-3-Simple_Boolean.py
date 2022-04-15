1859cc8520997c79d0d6aaac9f13bd534f074157
mdphx-mac <mdawsonphx@gmail.com>
2022-04-14 21:51:42 -0700

    Function Exercise #3: Simple Boolean

---
+++ 006-Python-Functions-Methods/6.48.12-Function-Exercise-3-Simple_Boolean.py
@@ -0,0 +1,10 @@
+#Coding Exercise #3 - Simple Boolean
+
+#Define a function called myfunc that takes in a Boolean value (True or False).
+#If True, return "Hello" and if False return "Goodbye".
+
+def myfunc(a):
+    if a == True:
+        return "Hello"
+    elif a == False:
+        return "Goodbye"

78b28574685399ccefb279d0d046ae37c7dae7aa
mdphx-mac <mdawsonphx@gmail.com>
2022-04-15 23:05:18 -0700

    folder for section coding exercises

---
+++ 006-Python-Functions-Methods/6.48-Function-Coding-Exercises/6.49.17-Function-Exercise-8-args.py
@@ -0,0 +1,7 @@
+# Coding Exercise #8 - *args
+
+# Define a function called myfunc that takes in an arbitrary number of arguments
+# and returns the sum of those arguments
+
+def myfunc(*args):
+    return sum(args)

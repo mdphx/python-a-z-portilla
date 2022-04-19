86b3082ac881c6552352a935f2a82b0f137529a6
mdphx-mac <mdawsonphx@gmail.com>
2022-04-18 21:55:37 -0700

    Homework Exercise #1: Volume of a Sphere

---
+++ 006-Python-Functions-Methods/6.57-Function-Homework/6.57.01-Exercise-Vol-of-Sphere.py
@@ -0,0 +1,10 @@
+# Write a function that computes the volume of a sphere
+# given its radius
+
+# The volume of a sphere is 4/3𝞹r³
+
+def vol(rad):
+    return (4/3)*(3.14)*(rad**3)
+
+#check
+print (vol(2))
